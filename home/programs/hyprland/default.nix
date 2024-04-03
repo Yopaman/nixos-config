45761c3eb382a7fa831fabf489c0c74a1c92a383
Yopaman <pabloinscriptions@gmail.com>
2024-04-03 14:44:11 +0200

    add png version of wallpaper

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -5,6 +5,7 @@
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   home.file.".config/hypr/mocha.conf".source = ./mocha.conf;
   home.file.".config/hypr/wallpaper.jpeg".source = ./wallpaper.jpeg;
+  home.file.".config/hypr/wallpaper.png".source = ./wallpaper.png;
   home.file.".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
   home.file.".config/hypr/hypridle.conf".source = ./hypridle.conf;
 }
