f764a1607c4526259049042a22d84cba159b51af
Yopaman <pabloinscriptions@gmail.com>
2024-04-13 00:02:33 +0200

    update system packages

--- flake.nix
+++ flake.nix
@@ -14,44 +14,44 @@
 
     # Shameless plug: looking for a way to nixify your themes and make
     nix-colors.url = "github:misterio77/nix-colors";
-
-    centerpiece.url = "github:friedow/centerpiece";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nix-colors,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-                # > Our main nixos configuration file <
-        modules = [
-	        ./nixos/configuration.nix
-	        home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.pablo = import ./home/home.nix;
-            home-manager.extraSpecialArgs = { inherit nix-colors; };
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , nix-colors
+    , ...
+    } @ inputs:
+    let
+      inherit (self) outputs;
+    in
+    {
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        extra-substituters = [
+          "https://nix-community.cachix.org"
         ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          # > Our main nixos configuration file <
+          modules = [
+            ./nixos/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.pablo = import ./home/home.nix;
+              home-manager.extraSpecialArgs = { inherit nix-colors; };
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+        };
       };
     };
-  };
 }
